d0034cc090ff62c524c040f6b386b117ba12a134
oniichaNj <oniichan@mrowr.me>
2015-06-12 23:45:59 +0200

    channel

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ import (
 func main() {
 	SERVER := "irc.swehack.org:6697"
 	NICK := "faggotbot"
+	CHANNEL := "#laidback"
 	bannedwords := []string{"=o", "xD", "facebook", "tumblr", "haha aa", "bajsar", "skiter", "haha mm", "kek", "pepsi max", "SSD är värdelöst", "dkn ingen fv", "9gag", "flash xss", "jag bor i stockholm", "steam", "löfven", "åkesson", "deepweb", "norton", "antivirus", "ebin", "yolo", "jålå", "yålå", "swag", "sväg", "swäg"}
 	responses := []string{"faggot detected", "nice meme", "episkt, helt enkelt episkt", "( ͡° ͜ʖ ͡°)", "(ノಠ益ಠ)ノ彡┻━┻ ", "varför kan jag inte suga alla dessa kukar samtidigt", "bajsar ner mig", "kräks på golvet", "skiter sönder", "helvete vad smart ich bin", "https://www.youtube.com/watch?v=8iO5-ic0Ug4", "https://i.imgur.com/O4gRAHW.gif", "https://i.imgur.com/Fo4eamA.jpg"}
 
@@ -24,6 +25,9 @@ func main() {
 		log.Fatal(err)
 	}
 	rand.Seed(time.Now().UnixNano())
+	i.AddCallback("001", func(e *irc.Event) {
+		i.Join(CHANNEL)
+	})
 	i.AddCallback("PRIVMSG", func(e *irc.Event) {
 		if e.Nick == i.GetNick() {
 			return
