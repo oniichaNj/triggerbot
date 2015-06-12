cbe9339368bf66e48c03526a2e32beca7681cb4e
redpois0n <red@cmail.nu>
2015-06-12 14:01:09 +0200

    Update main.go

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ import (
 func main() {
 	SERVER := "irc.swehack.org:6697"
 	NICK := "faggotbot"
-	bannedwords := []string{"facebook", "tumblr", "haha aa", "bajsar", "skiter", "haha mm", "kek", "pepsi max", "SSD är värdelöst", "dkn ingen fv", "9gag", "flash xss", "jag bor i stockholm", "steam", "löfven", "åkesson", "deepweb", "norton", "antivirus", "ebin", "yolo", "jålå", "yålå", "swag", "sväg", "swäg"}
+	bannedwords := []string{"=o", "xD", "facebook", "tumblr", "haha aa", "bajsar", "skiter", "haha mm", "kek", "pepsi max", "SSD är värdelöst", "dkn ingen fv", "9gag", "flash xss", "jag bor i stockholm", "steam", "löfven", "åkesson", "deepweb", "norton", "antivirus", "ebin", "yolo", "jålå", "yålå", "swag", "sväg", "swäg"}
 	responses := []string{"faggot detected", "nice meme", "episkt, helt enkelt episkt", "( ͡° ͜ʖ ͡°)", "(ノಠ益ಠ)ノ彡┻━┻ ", "varför kan jag inte suga alla dessa kukar samtidigt", "bajsar ner mig", "kräks på golvet", "skiter sönder", "helvete vad smart ich bin", "https://www.youtube.com/watch?v=8iO5-ic0Ug4", "https://i.imgur.com/O4gRAHW.gif", "https://i.imgur.com/Fo4eamA.jpg"}
 
 	i := irc.IRC(NICK, NICK)
