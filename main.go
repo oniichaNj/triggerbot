3482e78cce2bb93b64eed71d4cd73ca3f1ce89ba
oniichaNj <oniichan@mrowr.me>
2015-06-13 22:32:26 +0200

    case insensitive due to popular demand

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ func main() {
 	SERVER := "irc.swehack.org:6697"
 	NICK := "faggotbot"
 	CHANNEL := "#laidback"
-	bannedwords := []string{"=o", "xD", "facebook", "tumblr", "haha aa", "bajsar", "skiter", "haha mm", "kek", "pepsi max", "SSD är värdelöst", "dkn ingen fv", "9gag", "flash xss", "jag bor i stockholm", "steam", "löfven", "åkesson", "deepweb", "norton", "antivirus", "ebin", "yolo", "jålå", "yålå", "swag", "sväg", "swäg"}
+	bannedwords := []string{"=o", "java", "xD", "facebook", "tumblr", "haha aa", "bajsar", "skiter", "haha mm", "kek", "pepsi max", "SSD är värdelöst", "dkn ingen fv", "9gag", "flash xss", "jag bor i stockholm", "steam", "löfven", "åkesson", "deepweb", "norton", "antivirus", "ebin", "yolo", "jålå", "yålå", "swag", "sväg", "swäg"}
 	responses := []string{"faggot detected", "nice meme", "episkt, helt enkelt episkt", "( ͡° ͜ʖ ͡°)", "(ノಠ益ಠ)ノ彡┻━┻ ", "varför kan jag inte suga alla dessa kukar samtidigt", "bajsar ner mig", "kräks på golvet", "skiter sönder", "helvete vad smart ich bin", "https://www.youtube.com/watch?v=8iO5-ic0Ug4", "https://i.imgur.com/O4gRAHW.gif", "https://i.imgur.com/Fo4eamA.jpg"}
 
 	i := irc.IRC(NICK, NICK)
@@ -34,7 +34,7 @@ func main() {
 		}
 		s := false
 		for _, val := range bannedwords {
-			if strings.Contains(e.Message(), val) {
+			if strings.Contains(strings.ToUpper(e.Message()), strings.ToUpper(val)) {
 				s = true
 			}
 		}
